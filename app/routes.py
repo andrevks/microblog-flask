61afa0603a1669f8ad9e88730098382533841d52
andrevks <italiabk@gmail.com>
2021-09-29 14:26:51 -0400

    build: chapter 2 templates (v0.2)

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,7 @@
  so that Flask knows what logic to execute when 
  a client requests a given URL.
 '''
-
+from flask import render_template
 from app import app_obj
 
 
@@ -14,4 +14,23 @@ from app import app_obj
 @app_obj.route('/')
 @app_obj.route('/index')
 def index():
-  return "Hello, world! let\'s do this "
+  user = {'username':'Andre'}
+  posts = [
+    {
+      'author':{'username': 'John'},
+      'body': 'Beautiful day in Portland!'
+    },
+    {
+      'author':{'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    },
+    {
+      'author':{'username': 'Kaylee'},
+      'body': '“Happiness is found in doing, not merely possessing.” - Napoleon Hill, Think and Grow Rich'
+    },
+    {
+      'author':{'username': 'Ada'},
+      'body': '“Who you are, what you think, feel, and do, what you love—is the sum of what you focus on.” - Cal Newport, Deep Work'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
